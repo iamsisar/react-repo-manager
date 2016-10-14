c8adbf2ca072e487044b4e9f4732dfe87512a312
iamsisar <cesarecocito@gmail.com>
2016-10-14 18:42:07 +0200

    ok webpack.production, da aggiungere prefix e build html

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,30 +1,38 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-	entry : ['babel-polyfill','./main.js'],
-	output : {
-		path : './build/',
-		filename : 'index.js',
-	},
-	devServer:{
-		inline: true,
-		port: 3333,
-	},
+    entry: ['babel-polyfill', './main.js'],
+    output: {
+        path: './dist/',
+        filename: 'index.js',
+    },
+    devServer: {
+        inline: true,
+        port: 3333,
+    },
     module: {
-        loaders: [
-            {
+        loaders: [{
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015', 'stage-2']
                 }
-            },
-            {
-                test: /\.scss|css$/,
-                loaders: [ 'style', 'css?sourceMap&modules', 'sass?sourceMap' ]
-            },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
-
+            }, {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            },{
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            }, {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
+            }
         ],
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ]
 }
