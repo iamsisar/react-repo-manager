fe9cb12714e6400ff3bf92666f243cb44674f283
cescoc <cesarecocito@gmail.com>
2016-10-15 19:54:59 +0200

    autoprefixer via postcss - cant figure out production

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,18 @@ module.exports = {
             },
             {
                 test: /\.scss|css$/,
-                loaders: [ 'style', 'css?sourceMap&modules', 'sass?sourceMap' ]
+                loaders: [ 'style', 'css?sourceMap&modules', 'sass?sourceMap','postcss?sourceMap' ]
             },
+
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 
         ],
+    },
+    postcss: function () {
+        return [
+            require('autoprefixer')
+        ];
     }
+
 }
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: ['babel-polyfill', './main.js'],
@@ -11,28 +12,37 @@ module.exports = {
     },
     module: {
         loaders: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-2']
-                }
-            }, {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-            },{
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            }, {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&minetype=application/font-woff"
-            }, {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-2']
             }
-        ],
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader", "postcss-loader")
+        }, {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&minetype=application/font-woff"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        }],
     },
     plugins: [
-        new ExtractTextPlugin("style.css")
-    ]
+        new ExtractTextPlugin("style.css"),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+    postcss: function () {
+        return [
+            require('autoprefixer')
+        ];
+    }
 }
